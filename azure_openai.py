a717c26ac671422758d8b05877a407a2d0b7d911
Jupalaja <jupalaja@gmail.com>
2024-03-08 08:30:43 -0500

    first commit

---
+++ azure_openai.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+
+def get_completion_from_messages(system_message, user_message, model="gpt-4", temperature=0, max_tokens=500) -> str:
+
+    messages = [
+        {'role': 'system', 'content': system_message},
+        {'role': 'user', 'content': f"{user_message}"}
+    ]
+
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+        max_tokens=max_tokens, 
+    )
+    
+    return response.choices[0].message.content
+
+if __name__ == "__main__":
+    system_message = "You are a helpful assistant"
+    user_message = "Hello, how are you?"
+    print(get_completion_from_messages(system_message, user_message))
